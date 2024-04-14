7af85fb0286899998d296265a82611453bdbc79f
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-14 11:33:48 +0000

    0x00. Personal data Mandatory Task 1"

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ PII filter function implementation
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -15,3 +16,25 @@ def filter_datum(fields: List[str],
         message = re.sub(r'{}=.*?{}'.format(obs, separator),
                          '{}={}{}'.format(obs, redaction, separator), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self._fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self._fields,
+                                  self.REDACTION,
+                                  record.msg,
+                                  self.SEPARATOR)
+        record.msg = record.msg.replace(self.SEPARATOR,
+                                        "{} ".format(self.SEPARATOR))
+        return super().format(record)

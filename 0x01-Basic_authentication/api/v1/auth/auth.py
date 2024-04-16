f5835e2dc3a840677a3229b0c73626e3d8584062
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-16 19:00:51 +0000

    0x01. Basic authentication Mandatory Task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Basic Auth Implementation
+"""
+from flask import request
+
+
+class Auth:
+    """
+    Basic Authorization class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Auth requirement method
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Handles authorization header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Checks current user
+        """
+        return None

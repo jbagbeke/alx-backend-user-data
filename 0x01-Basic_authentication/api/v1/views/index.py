c9a237c9d95a7ffa1c806bbe9aa44813206cdf36
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-16 18:47:44 +0000

    0x01. Basic authentication Mandatory Task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized():
+def unauthorized() -> str:
     """ GET /api/v1/unauthorized
         Unauthorized users
     """

ff659c849622abb3a39989decdb63b02e3172b30
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-19 22:51:52 +0000

    0x02. Session authentication Mandatory Task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,10 +4,22 @@ Session authentication for the api
 """
 from api.v1.auth.auth import Auth
 from typing import TypeVar
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
     Session authentication class definition
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Session creation method
+        """
+        if not user_id or type(user_id) is not str:
+            return None
+        session_id = uuid4()
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+
+        return session_id

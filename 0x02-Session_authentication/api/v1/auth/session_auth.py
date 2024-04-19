c6c3b0fe76e0bca4dabf7356acf945263e102f06
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-19 22:41:02 +0000

    0x02. Session authentication Mandatory Task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Session authentication for the api
+"""
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+
+
+class SessionAuth(Auth):
+    """
+    Session authentication class definition
+    """
+    pass

0974972c44f3d647546b4b65225173ef53a8f5f2
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-23 20:36:04 +0000

    0x03. User authentication service Mandatory Task 4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Authorization authentication
+"""
+# from db import DB
+# from user import User
+import bcrypt
+
+
+class Auth:
+    """
+    Autorization class definition
+    """
+    def _hash_password(password: str) -> bytes:
+        """
+        Password hashing function using bcrypt
+        """
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        return hashed_password

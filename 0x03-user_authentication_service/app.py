50ff2108ced0fb3894b9821758f85f7bf0174312
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-24 22:50:51 +0000

    0x03. User authentication service Mandatory Task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """
 User authentication flask application
 """
-from flask import Flask, jsonify
+from flask import Flask, abort, request, jsonify
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -14,6 +16,24 @@ def app_root():
     Root path for the app
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """
+    Registers A user to the Database
+    """
+    user_data = request.form
+    email = user_data.get('email')
+    password = user_data.get('password')
+
+    if not email or not password:
+        abort(404)
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": str(email), "message": "user created"})
 
 
 if __name__ == "__main__":

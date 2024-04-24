0d24ef8657a453d0c1c457be2586ae93767c7f7c
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-24 22:09:20 +0000

    0x03. User authentication service Mandatory Task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,9 @@
 Authorization authentication
 """
 # from db import DB
-# from user import User
+from user import User
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
@@ -14,3 +16,21 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'),
                                     bcrypt.gensalt())
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers and adds user to the dataBase
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))

d2b4da2c4f2fc5ae0fa2f722234149da3caed87c
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-24 22:59:11 +0000

    0x03. User authentication service Mandatory Task 7

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,3 +34,16 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validates User login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if not user:
+                return False
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except Exception:
+            return False
